5a5a6521a58cf5fcca87ed8d74e27b41d332e742
TejasPatne <tejaspatne18@gmail.com>
2023-12-06 07:44:46 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(()=>{
+        console.log(`Server connected to MongoDB`);
+    }).catch((error)=>{
+        console.log(`Error occurred during database connection: ${error}`);
+    })
 
 const app = express();
 
