c78985e07c2c56c38442b43a4cbe4cbabd7707c3
TejasPatne <tejaspatne18@gmail.com>
2023-12-07 07:48:29 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -11,6 +13,25 @@ export const signup = async (req, res, next) => {
             message: "User created successfully"
         });
     } catch(error){
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser)  return next(errorHandler(404, 'User does not exists'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRETE);
+        const { password: hashedPassword, ...userInfoWithoutPassword} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hr
+        res
+            .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+            .status(200)
+            .json(userInfoWithoutPassword);
+    } catch (error) {
         next(error);
     }
 }
